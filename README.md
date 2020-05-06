93fa6d5ca6cbe48735677ce5349e4428eb7fdb6f
tomomo <tekuteku0501@gmail.com>
2020-05-06 16:58:47 +0900

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# ReactFinalFormSample
-Created with CodeSandbox
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles.css'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>React Final Form Samples</h1>
+      <p>React + ReactFinalForm + Typescript</p>
+    </div>
+  )
+}
+
+export default App
